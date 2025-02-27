483b65ee5db67b1c9c997d055efae55f7bfc7455
tleuenberger <tleuenberger@kpmg.com>
2025-02-27 10:58:20 +0100

    app is checking connection to db

--- TemplateProject/TemplateProject/App.xaml.cs
+++ TemplateProject/TemplateProject/App.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Configuration;
 using System.Data;
+using System.Data.SqlClient;
 using System.Windows;
 
 namespace TemplateProject;
@@ -9,4 +10,33 @@ namespace TemplateProject;
 /// </summary>
 public partial class App : Application
 {
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+
+        if (!CanConnectToDb("Server=(localdb)\\MSSQLLocalDB;Database=TestDB;"))
+        {
+            MessageBox.Show(
+                "Cannot connect to the database. Make sure to follow the instructions in the README file. The Application will now close.",
+                "Database Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            Shutdown();
+        }
+    }
+
+    private bool CanConnectToDb(string connString)
+    {
+        try
+        {
+            using (var conn = new SqlConnection(connString))
+            {
+                conn.Open();
+                return true;
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Database connection failed:\n{e.Message}");
+            return false;
+        }
+    }
 }
