3a186c86407d3b4e646cd89e34f6253be485653e
tleuenberger <tleuenberger@kpmg.com>
2025-02-27 08:31:45 +0100

    initial

---
+++ TemplateProject/create_db.ps1
@@ -0,0 +1,21 @@
+﻿$ErrorActionPreference = "Stop"
+
+function Invoke-SqlScript {
+    param (
+        [string]$Message,
+        [string]$ScriptPath,
+        [string]$DatabaseName = "DatabaseName"
+    )
+
+    Write-Host $Message
+    
+    $fullScriptPath = "$PSScriptRoot/$ScriptPath"
+    
+    & sqlcmd -S "(localdb)\MSSQLLocalDB" -d $DatabaseName -E -i $fullScriptPath
+}
+
+
+Invoke-SqlScript `
+  -Message "Creating DB DatabaseName" `
+  -ScriptPath "sql/database.sql" `
+  -DatabaseName "master" 
